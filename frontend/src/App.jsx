70215b3d456cbae92cac96485bfc1cdfde30f243
kirubel4 <kirubelayele2127@gmail.com>
2025-07-05 14:52:15 +0300

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import HomePage from "./page/home";
 import Nav from "./componets/Nav";
-import Footer from "./componets/Footer";
-import Landing from "./componets/Landing";
 function App() {
   return (
    <BrowserRouter>
       <div className="min-h-screen flex flex-col">
-          <Nav />
-          <Landing />
-          <Footer />
+          <HomePage />
       </div>
     </BrowserRouter>
   )
