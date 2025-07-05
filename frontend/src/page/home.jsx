7dfbcb356bdbfe0de79feb80eafb235e6d9aed24
kirubel4 <kirubelayele2127@gmail.com>
2025-07-05 14:49:27 +0300

    pages

---
+++ frontend/src/page/home.jsx
@@ -0,0 +1,14 @@
+import Footer from "../componets/Footer";
+import Nav from "../componets/Nav";
+import Landing from "../componets/Landing";
+
+function HomePage(){
+    return(
+        <>
+            <Nav />
+            <Landing />
+            <Footer />
+        </>
+    )
+}
+export default HomePage;
