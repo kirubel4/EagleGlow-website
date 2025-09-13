026fb90a3f6fc95ed0a5b54b29e8dbbfa0bb8ed6
kirubel4 <kirubelayele2127@gmail.com>
2025-09-13 16:35:48 +0300

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./componets/Nav";
 function App() {
   return (
    <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col bg-[#252525]">
           <HomePage />
       </div>
     </BrowserRouter>
